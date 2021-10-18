6a1e0301995247664a33addbd499d2157d05e78b
CarlosAstengo <69476270+CarlosAstengo@users.noreply.github.com>
2021-10-18 10:08:10 -0500

    Add files via upload

---
+++ App07-ImagePuzzle/App07-ImagePuzzle/Model/Img.swift
@@ -0,0 +1,16 @@
+//
+//  Img.swift
+//  App07-ImagePuzzle
+//
+//  Created by Carlos Astengo Macias on 18/10/21.
+//
+
+import SwiftUI
+
+struct Img : Identifiable {
+    
+    var id: Int
+    var image: String
+    
+    
+}
